2fd6f705e4f6f8f4026d0b2f9cfa9d1fc9ed0286
Giant <d.kirtsana@hotmail.com>
2016-12-20 09:46:03 +0700

    Add files via upload

---
+++ scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 8;
+	Vector3 targetPos;
+	GameObject ballObj;
+
+
+	// Use this for initialization
+	void Start () {
+		ballObj = GameObject.FindGameObjectWithTag ("Ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPos = Vector3.Lerp (gameObject.transform.position, ballObj.transform.position, Time.deltaTime * speed);
+		gameObject.transform.position = new Vector3 (22, targetPos.y, 0);	
+	}
+}
